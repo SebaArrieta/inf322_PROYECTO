0f192829f2ff56197f6957c5b0a4bf836407f5f3
Sebastian <sebastian.arrieta@usm.cl>
2024-11-03 16:13:28 -0300

    commit cambio de texto en el boton

--- client/src/components/Start-Ofrecer.js
+++ client/src/components/Start-Ofrecer.js
@@ -9,7 +9,7 @@ function StartOfrecer() {
       <button className="back-button" onClick={() => navigate('/')}>←</button>
       <h1>Servicios Domésticos a la puerta de tu casa</h1>
       <div className="button-container">
-        <button className="btn" onClick={() => navigate ('/ServiceForm')}>Prestar servicio</button>
+        <button className="btn" onClick={() => navigate ('/ServiceForm')}>Publicar un servicio</button>
         <button className="btn"onClick={() => navigate ('/mapa')}>Ver mapa</button>
       </div>
     </div>
