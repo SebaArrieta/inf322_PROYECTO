dcee9ac30a1eef691f7c77ca0a16000370c5a94b
Sebastian <sebastian.arrieta@usm.cl>
2024-11-12 21:39:46 -0300

    commit logo

--- client/src/components/Start.js
+++ client/src/components/Start.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../styles/Start.css';
 import {useNavigate} from 'react-router-dom';
+import logo from '../assets/ServiciosDomesticos.png';
 
 function Start() {
     const navigate = useNavigate();
   return (
     <div className="container">
+      <div className="logo-container">
+          <img src={logo} alt="Logo Servicios Domésticos" className="logo" />
+      </div>
       <h1>Servicios Domésticos a la puerta de tu casa</h1>
       <div className="button-container">
         <button className="btn" onClick={() => navigate ('/buscar')}>Buscar un servicio</button>
